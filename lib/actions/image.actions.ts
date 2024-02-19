d9b31edae2168173ae0bfabf21a1a2580a08d854
C-o-m-o-n <comon928@gmail.com>
2024-02-19 16:13:33 +0300

    edit models

--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -3,8 +3,8 @@
 import { revalidatePath } from "next/cache";
 import { connectToDatabase } from "../database/mongoose";
 import { handleError } from "../utils";
-import User from "../database/models/user.model.ts";
-import Image from "../database/models/image.model.ts";
+import User from "../database/models/user.model";
+import Image from "../database/models/image.model";
 import { redirect } from "next/navigation";
 
 import { v2 as cloudinary } from 'cloudinary'
