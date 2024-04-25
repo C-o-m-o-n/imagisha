01a02b85f1c67c000502b855e0f828480e7d64eb
Collins O. Odhiambo <comon928@gmail.com>
2024-04-25 03:25:16 +0000

    add images on homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -31,6 +31,14 @@ const Home = async ({ searchParams }: SearchParamProps) => {
           ))}
         </ul>
       </section>
+      <section className="sm:mt-12">
+        <Collection 
+          hasSearch={true}
+          images={images?.data}
+          totalPages={images?.totalPage}
+          page={page}
+        />
+      </section>
     </>
   )
 }
